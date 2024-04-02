20249af99b31f42e609ecad7e7e6325414e68c0c
Furkan Kutlu <ffrknktl@gmail.com>
2024-04-03 00:42:52 +0300

    aaa

---
+++ IntroductionToJava/helloworld.java
@@ -0,0 +1,12 @@
+package IntroductionToJava;
+public class helloworld {
+public static void main(String[] args) {
+    
+
+    System.out.println("Hello World");
+    System.out.println("aaaa");
+    int x = 5;
+    System.out.println("X="+x);
+}
+    
+}
